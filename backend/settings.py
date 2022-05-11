48aa1a87986d10ae9c3bd7423402ea5eb5d2853b
roshanrajeev <roshanrajeev2001@gmail.com>
2022-05-11 12:34:52 +0530

    Cors

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-fjwq@yu18y+q0r@vy+u=)^nx$_p_8#^42in-m(&4^v#orgd$o0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api'
 ]
 
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
